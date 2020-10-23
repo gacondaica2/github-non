20b18abb0c955c269835645e569896a88ef6d4f7
Nguyen Phong Linh <dankhanhpro@gmail.com>
2020-10-23 11:44:04 +0700

    update

--- pheptru.cpp
+++ pheptru.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
-#include <cstdilib>
+#include <cstdlib>
 using namespace std;
 
-int phepCong(){
-	return rand(1,100)+rand(1,100);
+void phepTru(){
+	int a=rand()%100+1;
+	int b=rand()%100+1;
+	cout<<"so a:" <<a<<endl;
+	cout<<"so b:" <<b<<endl;
+	cout<<"Ket qua la:" <<a - b<<endl;
 }
 bool kiemTra(int kq, int a,int b){
-	if(kq==(a+b))
+	if(kq==(a - b))
 		return true;
 	else return false;
+}
+void main(){
+	int kq,a,b;
+	phepTru();
+	cout<<"Nhap kq:"cin>>a;
+	cout<<endl;
+	cout<<"Nhap a:"cin>>a;
+	cout<<endl;
+	cout<<"Nhap b:"cin>>b;
+	cout<<endl;
+	if(kiemTra(kq,a,b)){
+		cout<<"Dung roi ne";
+	else cout<<"Thang ga`";
+	}
+	system("pause");
 }
